3465cd56d863199d15e6004f0e746ed2557e9467
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-02-16 23:55:53 +0000

    fix: update Jira ticket id regex to include `jira://` prefix

--- src/util/pattern.js
+++ src/util/pattern.js
@@ -1,5 +1,5 @@
 /**
- * This regex will match Jira ticket numbers in the notation XXX-NNNN
+ * This regex will match Jira ticket numbers in the notation `jira://XXX-NNNN`
  * where X is [a-zA-Z] and NNNN are all numbers in the range [0-9].
  */
-export const JiraTicketIdPattern = /[a-zA-Z-]*[0-9]/gm;
+export const JiraTicketIdPattern = /jira:\/\/[a-zA-Z-]*[0-9]*/gm;
